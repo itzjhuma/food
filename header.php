4f137cc6b8c39954bd051ea3217c14240104fb21
Firdous Begum <firdousbegum760@gmail.com>
2025-02-28 13:13:29 +0530

    header logo

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
     <body>
       <div class="container-fluid p-5 my-5 bg-primary text-blue">
 
-               <!-- <img src="img/logo3.png" class="mz-5">-->
+               <center><img src="logo1.png" style="width:120px; height:110px; " alt="centered logo1"></center>
                 <p class="mx-5 text-center"><b>Food with Fun</b></p>
                 <!--<a href="#"  class="nav-item nav-link texti text-blue "><i class="fa-solid fa-phone"></i> 0342 2560398 </a>
                 <a href="#"  class="nav-item nav-link text-blue"><i class="fa fa-mobile" aria-hidden="true color: #ffffff;"></i> +91 9932069297 </a>
