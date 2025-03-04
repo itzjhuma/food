0076b812f774d43be1e7b4f8f2b2e7302665b0d0
soumyagho <ghoshsoumyadip128@gmail.com>
2025-03-04 12:46:33 +0530

    img mod1

--- index.php
+++ index.php
@@ -49,20 +49,39 @@
         </div>
 
         <!-- Slider-->
+          <!--
 <div class="my-4">
 <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-  <!--<ol class="carousel-indicators">
-            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-          </ol>-->
+  
   <div class="carousel-inner">
-    <div class="carousel-item active"><img src="briyani.jpg" class="d-block w-100" alt="..."></div>
-    <div class="carousel-item active"><img src="briyani.jpg" class="d-block w-100"alt="..."></div>
-    <div class="carousel-item active"><img src="pizza.jpg" class="d-block w-100" alt="..."></div>
+    <div class="carousel-item active"><img src="p1.jpg" class="d-block w-100" alt="..."></div>
+    <div class="carousel-item active"><img src="p2.jpg" class="d-block w-100"alt="..."></div>
+    <div class="carousel-item active"><img src="p3.jpg" class="d-block w-100" alt="..."></div>
   </div>
   <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev"><span class="carousel-control-prev-icon" aria-hidden="true"></span><span class="sr-only">Previous</span></a><a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next"><span class="carousel-control-next-icon" aria-hidden="true"></span><span class="sr-only">Next</span></a></div>
+  </div> -->
+
+  <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100 H-50" src="p1.jpg" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100 H-50" src="pizza.jpg" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100 H-50" src="p3.jpg" alt="Third slide">
+    </div>
   </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
 
   <!-- *************Footer***************-->
 
