8ea90e3090c253a491a114d5afde1d11aa982ee1
itzjhuma <jhumabetal312@gmail.com>
2025-03-26 19:29:30 +0530

    portfolio include

---
+++ portfolio.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="eng">
+<head>
+</head>
+
+<meta charset="utf-8">
+<title>food- about us</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<!--- Favicon-->
+<link href="img/logo.png" rel="icon">
+
+<!--Google web font-->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link href="https://fonts.googleapis.com/css2?family=**Quicksand:wght@500**&display=swap" rel="stylesheet">
+
+<!---Font awesome-->
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
+<script src="https://kit.fontawesome.com/a7612e0a07.js" crossorigin="anonymous"></script>
+
+<!--css stylesheet-->
+<link href="css/style.css" rel="stylesheet">
+<link href="css/min.css" rel="stylesheet">
+
+<!--JS stylesheet-->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+</head><body>
+<div>
+<!-- ********* Header *********-->
+
+<?php include ("header.php");?>
+
+<!-- Navigator bar -->
+<div class="container-fluid bg-dark position-relative shadow">
+    <nav class="navbar navbar-expand-lg navbar-dark py-5 py-lg-0 px-0 px-lg-5 wow fadeIn" data-wow-delay="0.1s">
+    <a href="index.php" class="navbar-brand ms-4 ms-lg-0">
+                 
+      <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-start" id="navbarCollapse">
+        <div class="navbar-nav font-weight-bold py-0 text-center">
+          <a href="index.php" class="nav-item nav-link active"><i class="fa-solid fa-house-user"></i> HOME </a>
+          <a href="about.php" class="nav-item nav-link"><i class="fa-solid fa-address-card"></i> ABOUT US </a>
+          <a href="our portfolio.php" class="nav-item nav-link"><i class="fa-solid fa-circle-user"></i> Our Portfolio</a>
+          <a href="explore.php" class="nav-item nav-link"><i class="fa-solid fa-bowl-rice"></i>Explore Rice </a>
+          <a href="csr.php" class="nav-item nav-link"><i class="fa-solid fa-truck"></i>CSR</a>
+          <a href="contact.php" class="nav-item nav-link"><i class="fa-brands fa-centercode"></i> CONTACT US </a>
+          
+      </div>
+      </div>
+  
+    <!-- Social media icons -->
+      <div class="col-lg-6 px-5 text-right">
+                  <small>Follow us:</small>
+                  <a class="text-body ms-3" href=""><i class="fab fa-facebook-f"></i></a>
+                  <a class="text-body ms-3" href=""><i class="fab fa-twitter"></i></a>
+                  <a class="text-body ms-3" href=""><i class="fab fa-linkedin-in"></i></a>
+                  <a class="text-body ms-3" href=""><i class="fa-solid fa-magnifying-glass"></i>
+              </div>
+    
+    </nav>
+  </div>
+  
